695cfcbe60340dcad786c3da3d7e041ea6853a42
Abi Yudistira <a710230012@student.ums.ac.id>
2024-06-05 12:19:19 +0700

    Create day2.py
    day2_DataDiri

---
+++ day2.py
@@ -0,0 +1,7 @@
+nama = input('Masukkan nama: ')
+nim = input('Masukkan nim: ')
+angkatan = input('Masukkan angkatan: ')
+print('DATA DIRI')
+print('Hallo', nama)
+print('NIM mu adalah', nim)
+print('Kamu adalah angkatan', angkatan)
