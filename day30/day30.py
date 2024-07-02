cb161a0b65b05fd05223b6a84a9b8d058189f186
Abi Yudistira <a710230012@student.ums.ac.id>
2024-07-03 04:31:40 +0700

    Create day30.py
    day30

---
+++ day30/day30.py
@@ -0,0 +1,25 @@
+class Bus:
+  def move(self):
+    print("Bus lewat daratan.")
+
+class Helikopter:
+  def move(self):
+    print("Helikopter lewat udara.")
+
+class Kapal_Induk:
+  def move(self):
+    print("Kapal induk bertempur lewat lautan.")
+
+class Ikan_Paus:
+   def move(self):
+     print("Ikan paus mehrupakan hewan mamaliya laut")
+
+Bus = Bus()
+Helikopter = Helikopter()
+kapal_induk= Kapal_Induk()
+Ikan_Paus = Ikan_Paus()
+
+Bus.move()  
+Helikopter.move()  
+kapal_induk.move()
+Ikan_Paus.move()  
